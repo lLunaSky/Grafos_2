a1aa7c89d2b9c5802633f9486aca8207a2b4eb5b
Gabe-Faus <gabriel.faustino900@gmail.com>
2025-06-27 22:59:44 -0300

    Adicionei a biblioteca e fiz uma lógica melhor para ler os casais, porém ainda tem q melhorar a lógica

--- gale_shapley.py
+++ gale_shapley.py
@@ -1,39 +1,27 @@
 from proj_alum import alunos, projetos, participantes_projeto
-
+from collections import defaultdict #Para manipular dicionarios de forma mais tranquila
 """Aqui usaremos o Gale-Shapley eu decidi usar como se os alunos fossem os homens que pedem as mãos das mulheres, e elas serem os projetos pq faz mais sentindo na lógica que alunos podem escolher mais de um projeto. Esse algoritmo é uma base, eh preciso que sejam selecionados o sprojeto e ver se a nota dos alunos é sulficiente e se ha vagas. Mas esse algoritmo faz uma seleção. Poemos construir o resto a partir dele."""
 
 def asignacao(alunos, projetos, participantes_projeto):
-    #Estado atual
     alunos_livres = list(alunos.keys())
-    #chamamos participantes_projeto
-    propostas = {al: [] for al in alunos}
 
-    while alunos_livres:
-        aluno = alunos_livres[0]
-        
-        for projeto in alunos[aluno]:
-            if projeto not in propostas[aluno]:
-                propostas[aluno].append(projeto)
-                #Se o projeto esta livre ent da match
+    for aluno in alunos_livres:
+        preferencias = alunos[aluno][:-1]  
+        nota = alunos[aluno][-1]          
 
-                if projeto not in participantes_projeto.values():
-                    participantes_projeto[aluno] = projeto
-                    alunos_livres.pop(0)
-                    break
+        for projeto in preferencias:
+            if projeto not in projetos:
+                continue
+            else:
+                capacidade, nota_minima = projetos[projeto]
 
-                else:
-                    outro_aluno = [al for al, proj in participantes_projeto.items() if proj == projeto][0]
-                    if projetos[projeto].index(aluno) < projetos[projeto].index(outro_aluno):
-                        # Projeto prefere outro aluno
-                        participantes_projeto[aluno] = projeto
-                        alunos_livres.pop(0)
-                        alunos_livres.append(outro_aluno)
-                        del participantes_projeto[outro_aluno]
-                        break
-                    else:
-                        # Projeto prefere o atual
-         
-                        continue
+            if capacidade > 0 and nota >= nota_minima:
+                participantes_projeto[projeto].append(aluno)
+                projetos[projeto] = (capacidade - 1, nota_minima)
+                
+                break  
+
+    print(participantes_projeto, len(participantes_projeto))
     return participantes_projeto
 
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+alunos[aluno][0] : aluno
